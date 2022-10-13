936f09b907772ed63a59f6e40ee477f63872a273
Luis Carlos Cabezas Castro <luiscarloscabezascastro@gmail.com>
2022-10-13 06:24:07 -0500

    Curd Avance

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,13 +1,54 @@
 const producto = require("../models/productos")
 
-
 //Ver lista de producto
-exports.getProducts=(req,res,next)=>{
+exports.getProducts=async(req,res,next)=>{
+    const productos = await producto.find();
     res.status(200).json({
         success:true,
-        message: "En esta ruta usted podra ver todos los productos"
+        cantidad:productos.length,
+        productos
     })
 }
+
+// Ver productos por ID
+exports.getProductById = async(req,res,next)=>{
+    const product = await producto.findById(req.params.id)
+    if(!product){
+        return res.status(404).json({
+            success:false,
+            message: "No encontramos ese producto"
+        })
+    }
+    res.status(200).json({
+        success:true,
+        message: "Aque esta la info de los productos",
+        product
+    })
+}
+
+// Update a product
+exports.updateProduct = async (req,res,next)=>{
+    let product = await producto.findById(req.params.id)
+    if(!product){
+        return res.status(404).json({
+            success:false,
+            message: "No encontramos ese producto"
+        })
+    }
+
+    product = await producto.findByIdAndUpdate(req.params.id, req.body, {
+        new:true,
+        runValidators:true
+    });
+    res.status(200).json({
+        success:true,
+        message: "Producto actualizado correctamente",
+        product
+    })
+}
+
+//Eliminar un producto
+
 
 //Crear nuevo producto /api/productos
 exports.newProduct=async(req,res,next)=>{
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {getProducts, newProduct} = require("../controllers/productsController")
+const {getProducts, newProduct, getProductById, updateProduct, } = require("../controllers/productsController")
 
-router.route('/productos').get(getProducts)
-router.route('/producto/nuevo').post(newProduct);
+router.route('/productos').get(getProducts)// Listar productos
+router.route('/producto/nuevo').post(newProduct); //Crear producto
+router.route('/producto/:id').get(getProductById); //Ruta para consultar por ID
+router.route('/producto/:id').put(updateProduct); // Ruta para actualizar
 
 module.exports = router;
